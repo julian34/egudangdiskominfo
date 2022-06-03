54aac6544df028143269860eacea94bf4444a4ea
julian allen <julianallen111@gmail.com>
2022-06-03 14:47:53 +0900

    add Datatables

--- app/Views/main/layout.php
+++ app/Views/main/layout.php
@@ -58,16 +58,16 @@
                             alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">Administrator</a>
+                        <a href="#"
+                            class="d-block"><?= (user()->username != null) ? strtoupper(user()->username) : 'Administrator'; ?></a>
                     </div>
                 </div>
-
                 <!-- Sidebar Menu -->
                 <nav class="mt-2">
                     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                         data-accordion="false">
                         <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
+                with font-awesome or any other icon font library -->
                         <li class="nav-header">MASTER</li>
                         <li class="nav-item">
                             <a href="<?=site_url('kategori/index')?>" class="nav-link">
@@ -130,6 +130,19 @@
                             <a href="<?=site_url('barangmasuk/data')?>" class="nav-link">
                                 <i class="nav-icon fa fa-user-secret"></i>
                                 <p class="text">Permission</p>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+
+                <div class="user-panel mt-3 pb-3 mb-3 d-flex"></div>
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                        data-accordion="false">
+                        <li class="nav-item">
+                            <a href="<?=site_url('logout')?>" class="nav-link">
+                                <i class="nav-icon fas fa-sign-out-alt"></i>
+                                <p class="text">Logout</p>
                             </a>
                         </li>
                     </ul>
