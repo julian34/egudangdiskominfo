b7ae44e6ebec343a00113b709acd1c696cc6adba
julian allen <julianallen111@gmail.com>
2022-06-15 02:23:29 +0900

    Update

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -1,106 +1,78 @@
-<?= $this->extend($config->viewLayout) ?>
-<?= $this->section('main') ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<body class="hold-transition login-page">
-    <div class="login-box">
-        <!-- /.login-logo -->
-        <div class="card card-outline card-primary">
-            <div class="card-header text-center">
-                <a href="<?= base_url() ?>" class="h1"><b><?=lang('Auth.loginTitle')?></b></a>
-            </div>
-            <div class="card-body">
-                <p class="login-box-msg">Sign in to start your session</p>
-                <?= view('App\Auth\_message_block') ?>
-                <form action="<?= base_url('login') ?>" method="post">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>::. Login SIDIA .::</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
+    <link rel="stylesheet" href="<?=base_url() ?>/template/assets/style.css" />
+</head>
+
+<body>
+
+    <div class="modal-dialog text-center">
+        <div class="col-sm-8 main-section">
+            <div class="modal-content">
+
+
+                <div class="col-12 ellipse">
+                    <img src="<?=base_url() ?>/template/assets/ellipse.png" class="ellipse-img">
+                </div>
+
+                <div class="col-12 logo-img">
+                    <img src="<?=base_url() ?>/template/assets/logo-pemprov.png">
+                </div>
+
+                <div class="col-12 title-app">
+                    <h1>SIDIA LOGIN</h1>
+                </div>
+
+                <div class="col-12 auth-massage-block">
+                    <?= view('App\Auth\_message_block') ?>
+                </div>
+
+                <form class="col-12" action="<?= base_url('login') ?>" method="post">
                     <?= csrf_field() ?>
-                    <?php if ($config->validFields === ['email']): ?>
-                    <div class="input-group mb-3">
-                        <input type="email"
-                            class="form-control <?php if(session('errors.login')) : ?>is-invalid<?php endif ?>"
-                            placeholder="<?=lang('Auth.email')?>" name="login">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-envelope"></span>
-                            </div>
-                        </div>
-                        <div class="invalid-feedback">
-                            <?= session('errors.login') ?>
-                        </div>
-                    </div>
-                    <?php else: ?>
-                    <div class="input-group mb-3">
+                    <div class="form-group">
+                        <img src="<?=base_url() ?>/template/assets/ellipse.png" class="ellipse-input">
                         <input type="text"
                             class="form-control <?php if(session('errors.login')) : ?>is-invalid<?php endif ?>"
-                            placeholder="<?=lang('Auth.emailOrUsername')?>" name="login">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-envelope"></span>
-                            </div>
-                        </div>
+                            placeholder="Enter <?=lang('Auth.email')?>" name="login">
                         <div class="invalid-feedback">
                             <?= session('errors.login') ?>
                         </div>
                     </div>
-                    <?php endif; ?>
 
-                    <div class="input-group mb-3">
+                    <div class="form-group">
+                        <img src="<?=base_url() ?>/template/assets/ellipse.png" class="ellipse-input">
                         <input type="password"
-                            class="form-control  <?php if(session('errors.password')) : ?>is-invalid<?php endif ?>"
-                            placeholder="<?=lang('Auth.password')?>" name="password">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
-                            </div>
-                        </div>
+                            class="form-control <?php if(session('errors.login')) : ?>is-invalid<?php endif ?>"
+                            placeholder="Enter <?=lang('Auth.password')?>" name="password">
                         <div class="invalid-feedback">
                             <?= session('errors.password') ?>
                         </div>
                     </div>
-                    <div class="row">
-                        <?php if ($config->allowRemembering): ?>
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" id="remember" <?php if(old('remember')) : ?> checked
-                                    <?php endif ?>>
-                                <label for="remember">
-                                    <?=lang('Auth.rememberMe')?>
-                                </label>
-                            </div>
-                        </div>
-                        <?php endif; ?>
-                        <!-- /.col -->
-                        <div class="col-4">
-                            <button type="submit"
-                                class="btn btn-primary btn-block"><?=lang('Auth.loginAction')?></button>
-                        </div>
-                        <!-- /.col -->
-                    </div>
+                    <button class="btn"><i class="fas fa-sign-in-alt"></i> <?=lang('Auth.loginAction')?></button>
                 </form>
-
-                <?php if ($config->allowRegistration) : ?>
-                <p class="mb-1">
-                    <a href="<?= route_to('register') ?>" class="text-center"><?=lang('Auth.needAnAccount')?></a>
-                </p>
-                <?php endif; ?>
-                <?php if ($config->activeResetter): ?>
-                <p class="mb-0">
-                    <a href="<?= route_to('forgot') ?>" class="text-center"><?=lang('Auth.forgotYourPassword')?></a>
-                </p>
-                <?php endif; ?>
             </div>
-            <!-- /.card-body -->
+            <!-- End of Modal Content -->
         </div>
-        <!-- /.card -->
     </div>
-    <!-- /.login-box -->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js"></script>
 </body>
 
-<!-- jQuery -->
-<script src="<?= base_url() ?>/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="<?= base_url() ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="<?= base_url() ?>/dist/js/adminlte.min.js"></script>
-<?= $this->renderSection('pageScripts') ?>
-
-<?= $this->endSection() ?>
+</html>
